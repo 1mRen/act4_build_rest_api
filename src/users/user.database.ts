7bd577a4bd5fc14d07d1447521552620e95df2ce
Marc <mmarclawrence@gmail.com>
2025-02-18 01:22:22 +0800

    Update

--- src/users/user.database.ts
+++ src/users/user.database.ts
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 import {v4 as random} from "uuid";
 import fs from "fs";
 
+
 let users: Users = loadUsers()
 
 function loadUsers() : Users {
@@ -62,4 +63,59 @@ export const findByEmail = async (user_email: string): Promise<null | UnitUser> 
 
     const getUser = allUsers.find(result => user_email === result.email);
 
+    if (!getUser){
+        return null;
+    }
+
+    return getUser;
+};
+
+export const comparePassword = async (email : string, supplied_password : string) : Promise<null | UnitUser> => {
     
+    const user = await findByEmail(email)
+    
+    const decryptPassword = await bcrypt.compare(supplied_password, user!.password)
+    
+    if (!decryptPassword) {
+        return null
+    }
+
+    return user
+}
+
+export const update = async (id : string, updateValues : User) : Promise<UnitUser | null> => {
+    
+    const userExist = await findOne(id)
+
+    if (!userExist) {
+        return null
+    }
+
+    if(updateValues.password){
+        const salt = await bcrypt.genSalt(10)
+        const newPass = await bcrypt.hash(updateValues.password, salt)
+
+        updateValues.password = newPass
+    }
+
+    users[id] = {
+        ...userExist,
+        ...updateValues
+    }
+
+    saveUsers()
+
+    return users [id]
+}
+
+export const remove = async (id : string) : Promise<null | void> => {
+    const user = await findOne(id)
+
+    if (!user){
+        return null
+    }
+
+    delete user[id]
+
+    saveUsers()
+}
